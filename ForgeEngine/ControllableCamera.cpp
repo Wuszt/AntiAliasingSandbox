2417d0bac9fa7a54df8fd8b2cc77fec89ca8a129
Jakub Kuźnik (jakukuz389) <jakukuz389@student.polsl.pl>
2019-06-14 12:23:11 +0200

    Add moving using mouse wheel.

--- ForgeEngine/ControllableCamera.cpp
+++ ForgeEngine/ControllableCamera.cpp
@@ -9,16 +9,17 @@ ControllableCamera::~ControllableCamera()
 
 void ControllableCamera::Update()
 {
+    float horizontal = ((InputClass::GetKey(DIK_D) ? 1 : 0) - (InputClass::GetKey(DIK_A) ? 1 : 0)) * Time::GetDeltaTime() * 5.0f;
+    float vertical = ((InputClass::GetKey(DIK_W) ? 1 : 0) - (InputClass::GetKey(DIK_S) ? 1 : 0)) * Time::GetDeltaTime() * 5.0f;
+
     if (InputClass::IsMouseButtonDown(1))
     {
         XMFLOAT3 delta = InputClass::GetMouseDeltaPosition();
         float sensitivity = 0.001f;
         m_transform->RotateLocal(delta.y * sensitivity, 0.0f, 0.0f);
         m_transform->RotateGlobal(0.0f, delta.x * sensitivity, 0.0f);
+        vertical += delta.z * 0.025f;
     }
-
-    float horizontal = ((InputClass::GetKey(DIK_D) ? 1 : 0) - (InputClass::GetKey(DIK_A) ? 1 : 0)) * Time::GetDeltaTime() * 5.0f;
-    float vertical = ((InputClass::GetKey(DIK_W) ? 1 : 0) - (InputClass::GetKey(DIK_S) ? 1 : 0)) * Time::GetDeltaTime() * 5.0f;
 
     m_transform->Translate(horizontal, 0.0f, vertical);
 }
